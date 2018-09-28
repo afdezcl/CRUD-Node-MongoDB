8211912e23f84d182f0b0033b2bfea5ce32d3335
afdezcl <adrifdez_@hotmail.com>
2018-09-28 14:57:48 +0200

    Minor routes and connected to mongoDB

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+// connecting to data base
+mongoose.connect('mongodb://localhost:27017/crud-db', { useNewUrlParser: true })
+    .then(db => console.log("DB connected"))
+    .catch(err => console.log(err));
 
 // import routes
+const indexRoutes = require('./routes/index');
+
 
 // settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); 
+
+// middlewares
+app.use(express.urlencoded({extended: false})); // coded data from form to JSON
+
+
+// routes
+app.use('/', indexRoutes);
 
 
 // starting the server
