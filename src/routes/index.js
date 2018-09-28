0556e22bdf669dc7d589ca2a5ca514f0677b7039
afdezcl <adrifdez_@hotmail.com>
2018-09-29 01:28:38 +0200

    Design principal view with Bootstrap 4

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.post('/add', (req, res) => {
+    res.send('Hello Post');
+    console.log("Hello!");
+});
+
+
 module.exports = router;
