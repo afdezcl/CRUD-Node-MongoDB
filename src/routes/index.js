9274067707dbe97d3ca71b35a5f341cd6cd42167
afdezcl <adrifdez_@hotmail.com>
2018-09-28 15:57:46 +0200

    Request GET in root path, show index.ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// Request GET in root path
+router.get('/', (req, res) => {
+    res.render('index');
+});
 
 module.exports = router;
