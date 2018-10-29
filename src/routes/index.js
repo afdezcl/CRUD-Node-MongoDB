c0960c306d63d7ea3fed49dbcf2dd4930160fecb
afdezcl <adrifdez_@hotmail.com>
2018-10-29 18:18:55 +0100

    Partials in views, update task, change status of task, delete task. Work

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,6 +20,34 @@ router.post('/add', async (req, res) => {
 });
 
 
+router.get('/delete/:id_task', async (req, res) => { // (req, res) to do the manage with a function
+    const { id_task } = req.params;
+    await Task.deleteOne({_id: id_task});
+    res.redirect('/');
+});
 
+router.get('/edit/:id_task', async (req, res) => {
+    const { id_task } = req.params;
+    const task = await Task.findById(id_task);
+    res.render('edit', {
+        task
+    });
+});
+
+router.post('/update/:id_task', async (req, res) => {
+    const { id_task } = req.params;
+    await Task.updateOne({_id: id_task}, req.body);    
+    res.redirect('/');
+});
+
+
+
+router.get('/change_status/:id_task', async (req, res) => {
+    const { id_task } = req.params;
+    const task = await Task.findById(id_task);
+    task.status = !task.status;
+    await task.save();
+    res.redirect('/');
+});
 
 module.exports = router;
